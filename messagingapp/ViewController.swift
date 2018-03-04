e027e5a550e4b287c25ea95edb1ece4fcdf2f4e0
Becka <rebeccaellicehsiao@gmail.com>
2018-03-04 15:42:15 -0800

    Initial Commit

---
+++ messagingapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messagingapp
+//
+//  Created by Rebecca Hsiao on 2018/03/04.
+//  Copyright © 2018年 Rebecca Hsiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ messagingappTests/messagingappTests.swift
@@ -0,0 +1,36 @@
+//
+//  messagingappTests.swift
+//  messagingappTests
+//
+//  Created by Rebecca Hsiao on 2018/03/04.
+//  Copyright © 2018年 Rebecca Hsiao. All rights reserved.
+//
+
+import XCTest
+@testable import messagingapp
+
+class messagingappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
